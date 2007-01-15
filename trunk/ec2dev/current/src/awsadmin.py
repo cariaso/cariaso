21b23ce70f14bd49378d4bcc1a746bda3d3f67bf
cariaso <cariaso@84f4fed6-9a19-0410-82dd-e5acffe59b65>
2007-01-15 08:04:34 +0000

    now allows you to run and terminate images

--- trunk/ec2dev/current/src/awsadmin.py
+++ trunk/ec2dev/current/src/awsadmin.py
@@ -32,34 +32,51 @@ class Application:
  	AWS_ACCESS_KEY_ID = '<INSERT YOUR AWS ACCESS KEY ID HERE>'
         AWS_SECRET_ACCESS_KEY = '<INSERT YOUR AWS SECRET ACCESS KEY HERE>'
  
+	terminateIDs=[]
+	runIDs=[]
 	for o, a in opts:
             if o in ("-a", "--public"):
                 AWS_ACCESS_KEY_ID = a
             if o in ("-s", "--private"):
                 AWS_SECRET_ACCESS_KEY = a
 
+            if o in ("--terminate-instance", "--terminate"):
+                terminateIDs.append(a)
+            if o in ("--run-instance", "--run"):
+                runIDs.append(a)
+
+
         SECURITY_GROUP_NAME = "endog-ec2-example-rb-test-group"
         
         conn = EC2.AWSAuthConnection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
         
+	if terminateIDs:
+	    print "-- terminating %s --" % terminateIDs
+	    print conn.terminate_instances(terminateIDs)
+
+	if runIDs:
+	    print "-- running %s --" % runIDs
+	    for ami in runIDs:
+	        print conn.run_instances(ami)
+
+
         print "----- listing images -----"
         print conn.describe_images()
         
         print "----- listing instances -----"
         print conn.describe_instances()
         
-        #print "----- creating a security group -----"
-        #print conn.create_securitygroup(SECURITY_GROUP_NAME, "ec-example.rb test group")
-        
         print "----- listing security groups -----"
         print conn.describe_securitygroups
+
+        print "----- listing keypairs -----"
+        print conn.describe_keypairs()
         
+        #print "----- creating a security group -----"
+        #print conn.create_securitygroup(SECURITY_GROUP_NAME, "ec-example.rb test group")
         #print "----- deleting a security group -----"
         #print conn.delete_securitygroup(SECURITY_GROUP_NAME)
         
-        print "----- listing keypairs (verbose mode) -----"
-        conn.verbose = True
-        print conn.describe_keypairs()
 
 
 class Usage(Exception):
@@ -72,7 +89,10 @@ def main(argv=None):
     try:
         try:
             opts, args = getopt.getopt(argv[1:], "ha:s:", ["help", 
-				"public=", "private="])
+				"public=", "private=",
+				"terminate-instance=", "terminate=",
+				"run-instance=", "run=",
+				])
         except getopt.error, msg:
              raise Usage(msg)
 	Application(opts, args)
