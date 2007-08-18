40c78f816290f522563a27473bdd84c7982add64
cariaso <cariaso@84f4fed6-9a19-0410-82dd-e5acffe59b65>
2007-08-18 04:41:34 +0000

    Generalized a bit

--- trunk/genomics/aligner.pl
+++ trunk/genomics/aligner.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-use warnings;
+#use warnings;
 
 use Data::Dumper;
 use FileHandle;
@@ -15,7 +15,8 @@ use Bio::Tools::Run::Alignment::Clustalw;
 
 use Bio::PrimarySeq;
 
-my $fasta = FileHandle->new("/mnt/mnt/keepers/allsnps-20070802.fasta");
+my $allsnps_fn = $ARGV[0];
+my $fasta = FileHandle->new($allsnps_fn);
 my $seq_in = Bio::SeqIO->new('-fh' => $fasta,
 			     '-format' => 'fasta');
 
@@ -36,7 +37,7 @@ while (my $inseq = $seq_in->next_seq) {
     eval {
 	my ($rsid) = $inseq->display_id() =~ /\|(rs\d+)/;
 	
-	#next unless $rsid =~ /rs(4420638)/;
+	#next unless $rsid =~ /rs(28928896)/;
 	#next unless $rsid =~ /rs(6318|1800321|3788853|4420638)/;
 	print "checking $rsid\n";
 	
@@ -63,6 +64,10 @@ while (my $inseq = $seq_in->next_seq) {
 		$lookup{$read->id} = $read;
 	    }
 
+	    #my $www;
+	    #map {$www->{$_}++} split(/\s+/,join(' ',`cat wanted.txt`));
+
+
 	    my @exon_sets = $sim4->align(\@reads, $inseq);
 	    #print Dumper $exon_sets[0];
 	    my $reflen = $inseq->length;
@@ -71,34 +76,62 @@ while (my $inseq = $seq_in->next_seq) {
 		foreach my $exon($set->sub_SeqFeature){
 
 
+#		    my ($gi) = $exon->est_hit->seq_id =~ /(\d+)/;
+#		    if ($www->{$gi}) {
+# 			print "+";
+# 		    } else {
+# 			print "-";
+# 		    }
+# 		    print "\t";
+
+		    print $exon->percentage_id,"\t",($exon->est_hit->end - $exon->est_hit->start),"\t";
+
 		    print "hit of $reflen got ".$exon->start."\t".$exon->end."\t".$exon->strand;
-		    print "\tMatched ".$exon->est_hit->seq_id." \t".$exon->est_hit->start." \t".$exon->est_hit->end."\n";
+		    print "\tMatched ".$exon->est_hit->seq_id." \t".$exon->est_hit->start." \t".$exon->est_hit->end;
+		    
 
-		    if (($exon->start < $snppos) && ($exon->end > $snppos)) {
-
-
-
-			if ($exon->strand == 1) {
-			    push @fwd, $lookup{$exon->est_hit->seq_id};
-			    print "fwd ",$exon->est_hit->seq_id,"\n";
-			} else {
-			    print "rev ",$exon->est_hit->seq_id,"\n";
-			    my $fwd = $lookup{$exon->est_hit->seq_id};
-			    if (!$fwd) {
-				print STDERR "*****Failure on $rsid ",$exon->est_hit->seq_id,"\n";
-			    } else {
-
-				my $old = $fwd->seq;
-				my $new = join('', reverse(split(//,$fwd->seq)));
-				$new =~ tr/ATCG/TAGC/;
-				my $rev = Bio::PrimarySeq->new(-id => "r_".$fwd->display_id, 
-							       -seq => $new);
-				push @rev, $rev;
-			    }
-			}
-
-
+		    if (($exon->start > $snppos) || ($snppos > $exon->end)) {
+			print "\tdoes not straddle snp\n";
+			next;
 		    }
+
+		    if ($exon->percentage_id < 80) {
+			print "\trejected ",$exon->percentage_id,"% ident\n";
+			next;
+		    }
+		    
+		    my $length = $exon->est_hit->end - $exon->est_hit->start;
+		    if ( $length < 50) {
+			print "\trejected length=$length\n";
+			next;
+		    }
+
+
+
+
+
+
+		    if ($exon->strand == 1) {
+			push @fwd, $lookup{$exon->est_hit->seq_id};
+			print "\tfwd ",$exon->est_hit->seq_id;
+		    } else {
+			print "\trev ",$exon->est_hit->seq_id;
+			my $fwd = $lookup{$exon->est_hit->seq_id};
+			if (!$fwd) {
+			    print STDERR "*****Failure on $rsid ",$exon->est_hit->seq_id,"\n";
+			} else {
+
+			    my $old = $fwd->seq;
+			    my $new = join('', reverse(split(//,$fwd->seq)));
+			    $new =~ tr/ATCG/TAGC/;
+			    my $rev = Bio::PrimarySeq->new(-id => "r_".$fwd->display_id, 
+							   -seq => $new);
+			    push @rev, $rev;
+			}
+		    }
+		    
+
+		    print "\n";
 
 
 		}
@@ -113,7 +146,7 @@ while (my $inseq = $seq_in->next_seq) {
 		    $rout->write_aln($raln);
 		};
 		if ($@) {
-		    print STDERR "error with $rsid\n";
+		    print STDERR "error with $rsid $@\n";
 		}
 	    }
 	    
